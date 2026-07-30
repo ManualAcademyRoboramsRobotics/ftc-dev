25c57cbc4eaac4e82899f7807b7ea3c17e3b47a7
Isaac Glenzinski <isaacglenzinski@gmail.com>
2026-07-30 18:51:50 -0500

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class Constants {
+    //http://192.168.43.1:8080/dash
+
+    //////////////////////////////////////////////////////////////
+    // Constants
+    //////////////////////////////////////////////////////////////
+    public static double DISTANCE = 0.0;
+}
