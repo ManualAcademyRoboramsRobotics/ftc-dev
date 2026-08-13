ec8ffc9332edfbde9ba60f9e6fe24262df7f6906
Isaac Glenzinski <isaacglenzinski@gmail.com>
2026-08-13 18:59:57 -0500

    Fixing linear slide Opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearSlideOpmode.java
@@ -2,9 +2,11 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp(name = "Linear Slide OpMode")
@@ -16,7 +18,7 @@ public class LinearSlideOpmode extends OpMode {
     static double GEAR_CIRC_IN = DistanceUnit.INCH.fromMm(GEAR_CIRC_MM);
     static double ENCODER_TICKS = 103.8;
 
-    public DcMotor SlideMotor;
+    public DcMotorEx SlideMotor;
 
     public ElapsedTime timer;
 
@@ -26,13 +28,9 @@ public class LinearSlideOpmode extends OpMode {
     @Override
     public void init() {
         timer = new ElapsedTime();
-        SlideMotor = hardwareMap.get(DcMotor.class, "ls");
+        SlideMotor = hardwareMap.get(DcMotorEx.class, "ls");
         SlideMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         SlideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        SlideMotor.setTargetPosition(0);
-        SlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        telemetry.addData("Initialization", "Motor initialized");
     }
 
     /*
@@ -48,6 +46,10 @@ public class LinearSlideOpmode extends OpMode {
      */
     @Override
     public void start() {
+        SlideMotor.setTargetPosition(0);
+        SlideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        SlideMotor.setPower(0.25);
+        telemetry.addData("Initialization", "Motor initialized");
     }
 
     /*
@@ -59,6 +61,7 @@ public class LinearSlideOpmode extends OpMode {
         telemetry.addData("CurrentTicks", SlideMotor.getCurrentPosition());
         telemetry.addData("RequestedTicks", SlideMotor.getTargetPosition());
         telemetry.addData("Power", SlideMotor.getPower());
+        telemetry.addData("Current", SlideMotor.getCurrent(CurrentUnit.MILLIAMPS));
         telemetry.addData("RequestedDistance", Constants.DISTANCE);
         SlideMotor.setTargetPosition(distanceToTicks(Constants.DISTANCE));
 
